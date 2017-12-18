b111a52c615d0714223181cc7eb239aa31307a7b
Aaron Adames <aaron.adames.20@wheels-student.org>
2017-12-18 23:13:37 +0000

    work in progress

--- js/script.js
+++ js/script.js
@@ -5,7 +5,30 @@
 //GLOBAL VARIABLES
 
 
-
+var options = ['rock','scissors','paper'];
 
 // DOCUMENT READY FUNCTION BELOW
 
+
+
+$('document').ready(function() {
+  console.log('test'); 
+    $('#input-button').on('click', function(){
+        
+        console.log('click');
+        
+        // user answers
+        var user = $('#input-text').val().toLowerCase();
+        $('#user').html(user);
+        
+    
+        
+        //computer answers
+        var rand = Math.floor(Math.random() * (3));
+        var computer = options[rand];
+        $('#computer').html(computer);
+        
+        
+    });
+
+});
